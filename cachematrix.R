9bdf0574e99357b4c3668e3b8c508d346a656cb9
jfc <juanfe.castillo@gmail.com>
2014-11-23 23:26:25 +0100

    Assignment 2 done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##############################################################
+## 
+## makeCacheMatrix
+##  x: a matrix, optional
+##
+## it defines 4 methods:
+##  set: to assign a new matrix
+##  get: to get the current matrix
+##  setinverse: to set the new inverse matrix
+##  getinverse: to get the inverse matrix, already stored
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  ## set: it stores a matrix
+  ## 
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  ## get: it returns the stored matrix
+  ##
+  get <- function() x
+  
+  ## setinverse: it stores a matrix as the inverse one
+  ##
+  setinverse <- function(inv) inverse <<- inv
+  
+  ## getinverse: it returns the stored inverse matrix 
+  ##
+  getinverse <- function() inverse
+  
+  ## return the list of methods
+  ##
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
-
+##
+##############################################################
+## 
+## cacheSolve: to return the inverse of a matrix
+##  x: the matrix
+## 
+## It verifies if the matrix was already stored before
+## If it is identical to the stored version, then the stored inverse
+## matrix is returned.
+## If it wasn't stored before, or is different, then the inverse
+## matrix is calculated, stored and returned.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  im <- x$getinverse() # try to recover the inverse matrix
+  ## if it was stored before
+  if(!is.null(im)) { 
+    ## message("getting cached data authorized")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data, ...)
+  x$setinverse(im)
+  im
 }
